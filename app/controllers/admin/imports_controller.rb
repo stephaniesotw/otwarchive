7c6f7fc3658046c60f917cbe652f586fff45bdf4
Stephanie <stephaine@thepotionsmaster.net>
2013-02-12 12:57:18 -0600

    preping test efic import

--- app/controllers/admin/imports_controller.rb
+++ app/controllers/admin/imports_controller.rb
@@ -11,15 +11,19 @@ class Admin::ImportsController < ApplicationController
 
 
   def update
+    @nmi = MassImportTool.new()
+    @nmi.import_Data
+=begin
     unless params[:admin_import] == nil
-    @import_settings = params[:import_setting]
+    @import_settings = params[:admin_import]
     end
     
     @nmi = MassImportTool.new()
-    @nmi.populate(@admin_import)
+    @nmi.populate(@import_settings)
     #setflash; flash[:notice] = ts("Running Import Task  #{@import_settings[:import_short_name]}")
     @nmi.perform
-      
+
+=end
   end
 
 end
