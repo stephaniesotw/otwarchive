0f9d00b84f41e01f8e4162b34c7aa3bfb65d88a9
Stephanie <stephaine@thepotionsmaster.net>
2013-02-12 12:57:18 -0600

    preping test efic import

--- app/models/import_models/import_category.rb
+++ app/models/import_models/import_category.rb
@@ -1,3 +1,16 @@
-class ImportCategory < Struct.new(:old_id,:new_id,:description,:category_name,:locked,:parent_id)
+class ImportCategory
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+  extend ActiveModel::Translation
 
+
+  attr_accessor :old_id
+  attr_accessor :new_id
+  attr_accessor :description
+  attr_accessor :title
+  attr_accessor :category_name
+  attr_accessor :locked
+  attr_accessor :new_parent_id
+  attr_accessor :old_parent_id
 end
