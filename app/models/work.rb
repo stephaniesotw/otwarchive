fbdc65b2104491d63041678ffd040b0b1d1dac5a
Stephanie <stephanie@thepotionsmaster.net>
2013-02-23 16:35:03 -0600

    readded user validation checks that were removed for testing earlier

--- app/models/work.rb
+++ app/models/work.rb
@@ -502,7 +502,14 @@ class Work < ActiveRecord::Base
 
   # Save chapter data when the work is updated
   def save_chapters
-    self.chapters.first.save(:validate => false)
+    if self.chapters.count > 1
+      self.chapters.each do |c|
+        c.save(:validate=>false)
+      end
+    else
+      self.chapters.first.save(:validate => false)
+    end
+
   end
 
   # If the work is posted, the first chapter should be posted too
@@ -510,6 +517,12 @@ class Work < ActiveRecord::Base
     if self.posted_changed?
       self.chapters.first.posted = self.posted
       self.chapters.first.save
+      if self.chapters.count > 1
+        self.chapters.each do |c|
+          c.posted = self.posted
+          c.save
+        end
+      end
   end
 end
 
