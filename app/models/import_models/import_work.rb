971c5b9cbe53377ff96038922716a5f149089f64
Stephanie <stephaine@thepotionsmaster.net>
2013-03-14 14:35:36 -0500

    changed chapter construct function

--- app/models/import_models/import_work.rb
+++ app/models/import_models/import_work.rb
@@ -13,7 +13,7 @@ class ImportWork
   attr_accessor :summary
   attr_accessor :classes
   attr_accessor :old_user_id
-
+  attr_accessor :tag_list
   attr_accessor :new_pseud_id
   attr_accessor :source_archive_id
   attr_accessor :word_count
