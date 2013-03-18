daf594040361cea0a82086cac0dc97cde3bc0f67
Stephanie <stephanie@thepotionsmaster.net>
2013-03-18 01:39:40 -0500

    tweaks

--- app/models/import_models/mass_import_tool.rb
+++ app/models/import_models/mass_import_tool.rb
@@ -1295,7 +1295,19 @@ class MassImportTool
 
   end
 
-
+  # File actionpack/lib/action_view/helpers/text_helper.rb, line 266
+  def simple_format(text, html_options={}, options={})
+    text = '' if text.nil?
+    text = text.dup
+    start_tag = tag('p', html_options, true)
+    text = sanitize(text) unless options[:sanitize] == false
+    text = text.to_str
+    text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
+    text.gsub!(/\n\n+/, "</p>\n\n#{start_tag}")  # 2+ newline  -> paragraph
+    text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
+    text.insert 0, start_tag
+    text.html_safe.safe_concat("</p>")
+  end
   #update each record in source db reading the chapter text file importing it into content field
   def update_source_chapters()
     #select source chapters from database
